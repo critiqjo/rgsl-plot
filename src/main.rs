17bd73737e2e9efd8168d1e999821d77641806e6
critiqjo <john.ch.fr@gmail.com>
2015-06-09 11:54:56 +0530

    +plotting

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 extern crate rgsl;
+extern crate gnuplot;
 
 mod interpolator;
 
 use interpolator::Interpolator;
+use gnuplot::{Figure, Caption, Color};
 
 fn main() {
     let xa = &[1_f64, 2., 3.];
     let ya = &[6_f64, 2., 9.];
-    let mut interp_test = Interpolator::new_linear(xa, ya);
-    println!("interpolated: (1.5, {}), (2.5, {})", interp_test.eval(1.5), interp_test.eval(2.5));
+    let mut interp = Interpolator::new_polynomial(xa, ya);
+    let x = (10..31).map(|x| x as f64 / 10.0).collect::<Vec<_>>();
+    let y = x.iter().map(|x| interp.eval(*x)).collect::<Vec<_>>();
+    let mut fg = Figure::new();
+    fg.axes2d().lines(x.iter(), y.iter(), &[Caption("Interpolated"), Color("black")]);
+    fg.show();
 }
